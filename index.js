b964c9682044e63bee0049e61585bf5299eac855
Brian Zalewski <brian@megabyte.space>
2022-01-31 00:29:26 -0500

    ♻️ refactor(latest): latest

--- index.js
+++ index.js
@@ -1,67 +1,87 @@
 /* eslint require-atomic-updates: off */
 
-const {defaultTo, castArray} = require('lodash');
-const verifyGitHub = require('./lib/verify');
-const addChannelGitHub = require('./lib/add-channel');
-const publishGitHub = require('./lib/publish');
-const successGitHub = require('./lib/success');
-const failGitHub = require('./lib/fail');
+const { defaultTo, castArray } = require('lodash')
+const verifyGitHub = require('./lib/verify')
+const addChannelGitHub = require('./lib/add-channel')
+const publishGitHub = require('./lib/publish')
+const successGitHub = require('./lib/success')
+const failGitHub = require('./lib/fail')
 
-let verified;
+let verified
 
+/**
+ * @param pluginConfig
+ * @param context
+ */
 async function verifyConditions(pluginConfig, context) {
-  const {options} = context;
+  const { options } = context
   // If the GitHub publish plugin is used and has `assets`, `successComment`, `failComment`, `failTitle`, `labels` or `assignees` configured, validate it now in order to prevent any release if the configuration is wrong
   if (options.publish) {
     const publishPlugin =
-      castArray(options.publish).find((config) => config.path && config.path === '@semantic-release/github') || {};
+      castArray(options.publish).find((config) => config.path && config.path === 'semantic-release-github') || {}
 
-    pluginConfig.assets = defaultTo(pluginConfig.assets, publishPlugin.assets);
-    pluginConfig.successComment = defaultTo(pluginConfig.successComment, publishPlugin.successComment);
-    pluginConfig.failComment = defaultTo(pluginConfig.failComment, publishPlugin.failComment);
-    pluginConfig.failTitle = defaultTo(pluginConfig.failTitle, publishPlugin.failTitle);
-    pluginConfig.labels = defaultTo(pluginConfig.labels, publishPlugin.labels);
-    pluginConfig.assignees = defaultTo(pluginConfig.assignees, publishPlugin.assignees);
+    pluginConfig.assets = defaultTo(pluginConfig.assets, publishPlugin.assets)
+    pluginConfig.successComment = defaultTo(pluginConfig.successComment, publishPlugin.successComment)
+    pluginConfig.failComment = defaultTo(pluginConfig.failComment, publishPlugin.failComment)
+    pluginConfig.failTitle = defaultTo(pluginConfig.failTitle, publishPlugin.failTitle)
+    pluginConfig.labels = defaultTo(pluginConfig.labels, publishPlugin.labels)
+    pluginConfig.assignees = defaultTo(pluginConfig.assignees, publishPlugin.assignees)
   }
 
-  await verifyGitHub(pluginConfig, context);
-  verified = true;
+  await verifyGitHub(pluginConfig, context)
+  verified = true
 }
 
+/**
+ * @param pluginConfig
+ * @param context
+ */
 async function publish(pluginConfig, context) {
   if (!verified) {
-    await verifyGitHub(pluginConfig, context);
-    verified = true;
+    await verifyGitHub(pluginConfig, context)
+    verified = true
   }
 
-  return publishGitHub(pluginConfig, context);
+  return publishGitHub(pluginConfig, context)
 }
 
+/**
+ * @param pluginConfig
+ * @param context
+ */
 async function addChannel(pluginConfig, context) {
   if (!verified) {
-    await verifyGitHub(pluginConfig, context);
-    verified = true;
+    await verifyGitHub(pluginConfig, context)
+    verified = true
   }
 
-  return addChannelGitHub(pluginConfig, context);
+  return addChannelGitHub(pluginConfig, context)
 }
 
+/**
+ * @param pluginConfig
+ * @param context
+ */
 async function success(pluginConfig, context) {
   if (!verified) {
-    await verifyGitHub(pluginConfig, context);
-    verified = true;
+    await verifyGitHub(pluginConfig, context)
+    verified = true
   }
 
-  await successGitHub(pluginConfig, context);
+  await successGitHub(pluginConfig, context)
 }
 
+/**
+ * @param pluginConfig
+ * @param context
+ */
 async function fail(pluginConfig, context) {
   if (!verified) {
-    await verifyGitHub(pluginConfig, context);
-    verified = true;
+    await verifyGitHub(pluginConfig, context)
+    verified = true
   }
 
-  await failGitHub(pluginConfig, context);
+  await failGitHub(pluginConfig, context)
 }
 
-module.exports = {verifyConditions, addChannel, publish, success, fail};
+module.exports = { addChannel, fail, publish, success, verifyConditions }
